88681a64eaa66e43c149876985d9fe4bf6a6df8f
Nightfuury <rupsri1996@gmail.com>
2022-08-19 18:00:48 +0530

    server

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.resolve(`${__dirname}`)));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname,'index.html'));
+app.get('*', (req, res, next) => {
+    res.sendFile(path.resolve(`${__dirname}/index.html`));
 });
 
-
-app.listen(7953);
+app.listen(7953)
+  .on('error', error => {
+    console.log(error);
+  })
+  .on('listening', () => {
+    console.log(`listening on port ${port}`);
+  });
